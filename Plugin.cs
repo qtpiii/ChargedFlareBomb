02ae60b78b451d2cd55f7cf355c0796887c20726
itssmuug <lefacef@gmail.com>
2024-07-14 10:31:13 -0600

    tried IL hooking to make arti able to craft charged flare bombs. couldnt

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BepInEx;
 using UnityEngine;
 using Fisobs.Core;
-using BepInEx.Logging;
 using static Pom.Pom;
+using MoreSlugcats;
+using MonoMod.Cil;
+using System.Reflection.Emit;
+using Mono.Cecil;
 
 namespace ChargedFlareBomb
 {
@@ -22,12 +21,49 @@ namespace ChargedFlareBomb
             {
                 RegisterFisobs();
                 RegisterPOM();
+
+                //IL.Player.SwallowObject += ArtiCraftChargedFlare;
+
+                //On.Player.SwallowObject += Player_SwallowObject;
             }
             catch (Exception ex)
             {
                 Logger.LogError(ex);
             }
         }
+
+        //i tried lol
+
+        //private void ArtiCraftChargedFlare(ILContext il)
+        //{
+        //    ILCursor cursor = new(il);
+        //    cursor.GotoNext(
+        //        x => x.MatchLdarg(0),
+        //        x => x.MatchLdfld<UpdatableAndDeletable>("room"),
+        //        x => x.MatchLdfld<Room>("game"),
+        //        x => x.MatchCallvirt<RainWorldGame>("GetNewID"),
+        //        x => x.MatchNewobj<AbstractPhysicalObject>(),
+        //        x => x.MatchStloc(0),
+        //        x => x.MatchLdarg(0),
+        //        x => x.MatchLdcI4(1),
+        //        x => x.MatchCall<Player>("SubtractFood")
+        //        );
+        //    cursor.Index += 8;
+        //    cursor.Emit(OpCodes.Br_S, );
+        //}
+
+        //private void Player_SwallowObject(On.Player.orig_SwallowObject orig, Player self, int grasp)
+        //{
+        //    orig(self, grasp);
+        //    if (ModManager.MSC && self.SlugCatClass == MoreSlugcatsEnums.SlugcatStatsName.Artificer && self.FoodInStomach > 0)
+        //    {
+        //        if (self.abstractPhysicalObject.type == AbstractPhysicalObject.AbstractObjectType.Lantern)
+        //        {
+        //            self.abstractPhysicalObject = new AbstractPhysicalObject(self.room.world, ChargedFlareBombFisob.AbstractChargedFlareBomb, null, self.room.GetWorldCoordinate(self.mainBodyChunk.pos), self.room.game.GetNewID());
+        //            self.SubtractFood(1);
+        //        }
+        //    }
+        //}
 
         public void RegisterFisobs()
         {
